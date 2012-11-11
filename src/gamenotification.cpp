88fe0e6d6b2e6a303a8c51c7ae95c61106db6dd2
Teemu Erkkola <teemu.erkkola@iki.fi>
2012-11-11 21:08:36 +0200

    Fixed notification text rendering

--- src/gamenotification.cpp
+++ src/gamenotification.cpp
@@ -19,26 +19,21 @@ GameNotification::GameNotification(GameWorld* world, const std::string& text,
 {
   float width = 0;
   float height = size * 1.5f;
+  float const renderMultiplier = 4;
   
-  glhckText* textObject = glhckTextNew(800, height);
+  glhckText* textObject = glhckTextNew(512, 512);
   unsigned int font = glhckTextNewFont(textObject, "fonts/DejaVuSans.ttf");
   glhckTextColor(textObject, 255, 255, 255, 255);
-  glhckTextDraw(textObject, font, size, 0, size, text.data(), &width);
+  glhckTextDraw(textObject, font, size*renderMultiplier, 0, size*renderMultiplier, text.data(), &width);
 
-  glhckText* shadowObject = glhckTextNew(800, height);
-  unsigned int font2 = glhckTextNewFont(shadowObject, "fonts/DejaVuSans.ttf");
-  glhckTextColor(shadowObject, 0, 0, 0, 255);
-  glhckTextDraw(shadowObject, font2, size - 1, 1, size, text.data(), nullptr);
-  
-  glhckRtt* rtt = glhckRttNew(width, height, GLHCK_RTT_RGBA);
+  glhckRtt* rtt = glhckRttNew(width, height*renderMultiplier, GLHCK_RTT_RGBA);
   glhckRttBegin(rtt);
   glhckClear();
-  glhckTextRender(shadowObject);
   glhckTextRender(textObject);
   glhckRttFillData(rtt);
   glhckRttEnd(rtt);
   
-  o = glhckSpriteNew(glhckRttGetTexture(rtt), width, height);
+  o = glhckSpriteNew(glhckRttGetTexture(rtt), width/renderMultiplier, height);
   glhckObjectMaterialFlags(o, GLHCK_MATERIAL_ALPHA);
   glhckObjectPositionf(o, position.x, position.y, 0);
   
@@ -66,8 +61,8 @@ void GameNotification::update(const float delta)
     float scale = lerp(1, 2, progress);
     glhckObjectScalef(o, scale, scale, 1);
     
-    unsigned char opacity = lerp(255, 128, progress*progress);
-    //glhckObjectColorb(o, 255,255,255, opacity);
+    unsigned char opacity = lerp(255, 128, progress);
+    glhckObjectColorb(o, 255,255,255, opacity);
   }
   else
   {
